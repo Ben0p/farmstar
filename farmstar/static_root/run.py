3764bc722a92dccd187f96ba216c31733d30b832
Ben0p <skittle7ben@msn.com>
2018-01-20 18:51:21 +0800

    20180120

--- farmstar/static_root/run.py
+++ farmstar/static_root/run.py
@@ -5,30 +5,15 @@ import sys
 import sqlite3
 import datetime
 
-checklist = [[importConfig, "Configuration import"],
-             ['getDB()', "Get database name"],
-             ['DBfile', "Database file"],
-             ['getComport', "Get comport"],
-             ['testComport', "Testing comport"],
-             ]
-
-
-def run():
-    for each_check in checklist:
-        check_function = each_check[0]
-        check_name = each_check[1]
-        if check_function() == False:
-            print("%s.....[Fail]" %(check_name))
-        else:
-            print("%s.....[OK]" %(check_name))
-             
+ 
 
 #Import Config
-def importConfig():
-    try:
-        import config
-    except:
-        return(False)
+try:
+    import config
+except:
+    print("Import config......[Fail]")
+else:
+    print("Import config......[OK]")
 
 #Database name from config check
 def getDB():
@@ -46,55 +31,49 @@ def DBfile():
 #Serial Port config
 def getComport():
     try:
+        global comport
         comport = config.comport
     except:
         return(False)
 
 #Serial port connection
-def testComport():
-    return(False)
+def testComport():    
+    try:
+        ser = serial.Serial(comport,9600,timeout=1.5)
+        ser.readline().decode("utf-8")
+    except:
+        return(False)
 
-##    try:
-##        if(ser == None or lines[0] == ''):
-##            ser = serial.Serial(comport,9600,timeout=1.5)
-##            print("Reconnecting to %s" % (comport))
-##        line = ser.readline().decode("utf-8") # Read the entire string
-##        lines = line.rstrip('\n\r').split(",")
-##        try:
-##            livePos(lines)
-##            #logDB(lines)
-##        except:
-##            print("Live position fail")
-##    except:
-##        if(not(ser == None)):
-##            ser.close()
-##            ser = None
-##            print("Disconnecting")
-##        print("No Connection to %s" % (comport))
-##        time.sleep(2)
+#Test the GPS data stream
+def serialTest():
+    try:
+        ser = serial.Serial(comport,9600,timeout=1.5)
+        for _ in range(5):
+            line = ser.readline().decode("utf-8")
+            sentence = line.rstrip('\n\r').split(",")
+            #Do the checksum check or sum thing
+            
+    except:
+        return(False)
     
 
-'''
-Do a pre-flight test in a linux boot style ...[OK]
+#List of functions and description (checks)
+checklist = [[getDB, "Get database name"],
+             [DBfile, "Database file"],
+             [getComport, "Get comport"],
+             [testComport, "Testing comport"],
+             [serialTest, "Validate gps data"],
+             ]
 
-1 - Check if there is a config file (then import)
-    - else prompt to run setup
-    
-2 - Check for database and connect
-    - else prompt to run setup
-
-3 - Check serial port
-    - loop a prompt "Connecting... (ctl+c to cancel)"
-
-4 - Check time difference between GPS and device
-    - Error on the time, automatically syncronize?
-
-5 - Check GPS data and all the shit
-
-6 - Whatever else?
-
-99 - Once pre-flight completed, display static info [not scrolling]
-
-'''
+# running through checks and printing result
+def run():
+    for each_check in checklist:
+        check_function = each_check[0]
+        check_name = each_check[1]
+        if check_function() == False:
+            print("%s.....[Fail]" %(check_name))
+        else:
+            print("%s.....[OK]" %(check_name))
+            
 
 run()
